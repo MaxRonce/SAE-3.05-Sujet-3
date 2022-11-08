e56ac3dd7ff5fd1fe14ab20af20d352624f4d7e9
Lnoel <louisnoel45000@gmail.com>
2022-11-08 16:06:19 +0100

    création des différentes tables de la BD

---
+++ sql/table.sql
@@ -0,0 +1,72 @@
+DROP DATABASE IF EXISTS KAIRO;
+CREATE DATABASE KAIRO;
+USE KAIRO;
+DROP TABLE IF EXISTS REPONSEUSER;
+DROP TABLE IF EXISTS REPONSEQUESTION;
+DROP TABLE IF EXISTS QUESTION;
+DROP TABLE IF EXISTS TYPEQUESTION;
+DROP TABLE IF EXISTS QUESTIONNAIRE;
+DROP TABLE IF EXISTS USERS;
+
+
+CREATE TABLE USERS(
+    idUser INT(9),
+    prof boolean,
+    PRIMARY KEY (idUser)
+);
+
+
+CREATE TABLE QUESTIONNAIRE(
+    idQuestionnaire INT(9),
+    nom VARCHAR(500),
+    descr VARCHAR(500),
+    idUser INT(9),
+    PRIMARY KEY (idQuestionnaire)
+);
+
+
+CREATE TABLE TYPEQUESTION(
+    idType INT(9),
+    nomType VARCHAR(500),
+    PRIMARY KEY (idType)
+);
+
+
+CREATE TABLE QUESTION(
+    idQuestion INT(9),
+    question VARCHAR(500),
+    template VARCHAR(500),
+    valeurPoint INT(9),
+    pointNegatif boolean,
+    idQuestionnaire INT(9),
+    idType INT(9),
+    PRIMARY KEY (idQuestion)
+);
+
+
+CREATE TABLE REPONSEQUESTION(
+    idReponse INT(9),
+    reponse VARCHAR(500),
+    valide boolean,
+    idQuestion INT(9),
+    PRIMARY KEY (idReponse)
+    );
+
+
+CREATE TABLE REPONSEUSER(
+    idUser INT(9),
+    idQuestion INT(9),
+    reponse VARCHAR(500),
+    PRIMARY KEY (idQuestion, idUser)
+);
+
+
+ALTER TABLE QUESTION        ADD FOREIGN KEY (idQuestionnaire)   REFERENCES QUESTIONNAIRE(idQuestionnaire);
+ALTER TABLE QUESTION        ADD FOREIGN KEY (idType)            REFERENCES TYPEQUESTION(idType);
+ALTER TABLE QUESTIONNAIRE   ADD FOREIGN KEY (idUser)            REFERENCES USERS(idUser);
+ALTER TABLE REPONSEUSER     ADD FOREIGN KEY (idUser)            REFERENCES USERS(idUser);
+ALTER TABLE REPONSEUSER     ADD FOREIGN KEY (idQuestion)        REFERENCES QUESTION(idQuestion);
+ALTER TABLE REPONSEQUESTION ADD FOREIGN KEY (idQuestion)        REFERENCES QUESTION(idQuestion);
+
+
+
