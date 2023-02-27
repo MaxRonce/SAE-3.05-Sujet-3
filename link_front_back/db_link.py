38d446e0b9d53dacee97e72c7be56e553db494be
Lnoel <louisnoel45000@gmail.com>
2023-02-27 11:46:33 +0100

    bouton login fix avec css propre

--- link_front_back/db_link.py
+++ link_front_back/db_link.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from .app import login_manager
 
-login, passwd, serveur, bd = "antoninreydet", "root", "localhost", "KAIRO"
+login, passwd, serveur, bd = "root", "aled", "localhost", "KAIRO"
 engine = create_engine('mysql+mysqldb://'+login+':'+passwd+'@'+serveur+'/'+bd)
 
 ses = Session(engine)
