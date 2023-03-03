14e52d17af0a4352c8d965ea08bf77e75751df83
Yrow0 <anto28700@gmail.com>
2023-03-03 14:10:04 +0100

    question editing working + some fixes

--- link_front_back/setupdb.py
+++ link_front_back/setupdb.py
@@ -2,15 +2,15 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-login, passwd, serveur, bd = "root", "ronceray", "localhost", "kairo"
+login, passwd, serveur, bd = "antoninreydet", "root", "localhost", "kairo"
 engine = create_engine('mysql+mysqldb://' + login + ':' + passwd + '@' + serveur + '/' + bd)
 
 ses = Session(engine)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-Type = Base.classes.typequestion
-User = Base.classes.users
-Questionnaire = Base.classes.questionnaire
-Question = Base.classes.question
-RepQuestion = Base.classes.reponsequestion
+Type = Base.classes.TYPEQUESTION
+User = Base.classes.USERS
+Questionnaire = Base.classes.QUESTIONNAIRE
+Question = Base.classes.QUESTION
+RepQuestion = Base.classes.REPONSEQUESTION
